93453af54cc35902dcd9c095f1a0b2dbbfea7af8
banedj11 <djbane@live.com>
2022-08-21 22:36:08 +0200

    commiting

--- src/main/java/com/djuka/fizzbuzzapi/FizzBuzzResource.java
+++ src/main/java/com/djuka/fizzbuzzapi/FizzBuzzResource.java
@@ -1,11 +1,13 @@
 package com.djuka.fizzbuzzapi;
 
 import com.djuka.fizzbuzzapi.error.InvalidParameterException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FizzBuzzResource {
 
     private static final String ONLY_NUMBER_REGEX = "[0-9]+";
+    @Autowired
     private final FizzbuzzService fizzbuzzService;
 
     public FizzBuzzResource(FizzbuzzService fizzbuzzService) {
--- src/test/java/com/djuka/fizzbuzzapi/FizzBuzzResourceIntTest.java
+++ src/test/java/com/djuka/fizzbuzzapi/FizzBuzzResourceIntTest.java
@@ -1,33 +1,68 @@
 package com.djuka.fizzbuzzapi;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.event.annotation.BeforeTestClass;
+import org.springframework.test.context.event.annotation.BeforeTestMethod;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.hamcrest.core.Is.is;
 
 @ExtendWith(SpringExtension.class)
 @WebMvcTest
+@ContextConfiguration
 class FizzBuzzResourceIntTest {
 
+    @Autowired
+    private WebApplicationContext webApplicationContext;
     @Autowired
     private MockMvc mvc;
 
     @MockBean
     private FizzbuzzService fizzbuzzService;
 
+    @BeforeEach
+    public void setup() throws Exception    {
+        this.mvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
     @Test
     void whenParamIsNumber_StatusOk() throws Exception {
-        mvc.perform(get("/fizzbuzz?entry=15"))
+        this.mvc.perform(get("/fizzbuzz?entry=15").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andDo(print())
-                .andExpect(jsonPath("$.output", is("fizzbuzz")));
+                .andExpect(jsonPath("$.output").value("fizzbuzz"));
+    }
+
+    @Test
+    void whenParamIsNotNumber_StatusBadRequest() throws Exception {
+        this.mvc.perform(get("/fizzbuzz?entry=a").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andDo(print());
+    }
+
+    @Test
+    void whenParamIsEmpty_StatusOk() throws Exception {
+        String all = "[1, 2, fizz, 4, buzz, fizz, 7, 8, fizz, buzz, 11, fizz, 13, 14, fizzbuzz, 16, 17, fizz, 19, buzz, fizz, 22, 23, fizz, buzz, 26, fizz, 28, 29, fizzbuzz, 31, 32, fizz, 34, buzz, fizz, 37, 38, fizz, buzz, 41, fizz, 43, 44, fizzbuzz, 46, 47, fizz, 49, buzz, fizz, 52, 53, fizz, buzz, 56, fizz, 58, 59, fizzbuzz, 61, 62, fizz, 64, buzz, fizz, 67, 68, fizz, buzz, 71, fizz, 73, 74, fizzbuzz, 76, 77, fizz, 79, buzz, fizz, 82, 83, fizz, buzz, 86, fizz, 88, 89, fizzbuzz, 91, 92, fizz, 94, buzz, fizz, 97, 98, fizz, buzz]";
+        this.mvc.perform(get("/fizzbuzz?entry=").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andDo(print())
+                .andExpect(jsonPath("$.output").value(all));
+
     }
 }
